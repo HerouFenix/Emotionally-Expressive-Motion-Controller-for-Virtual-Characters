b1fea5114e002525812c16d1750af64b1f0bbca8
HerouFenix <diogo0408@gmail.com>
2022-03-21 15:17:42 +0000

    emotion classifier v2

--- emotion_classifier/emotion_classifier.py
+++ emotion_classifier/emotion_classifier.py
@@ -1,11 +1,10 @@
-import math
 import numpy as np
 import os.path
 
 import pandas as pd
 
 import xgboost as xgb
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 
 
 xgb.set_config(verbosity=0)
@@ -13,37 +12,20 @@ xgb.set_config(verbosity=0)
 class EmotionClassifier():
     def __init__(self):
         self._model_p = xgb.XGBRegressor(verbosity=0)
-        self._model_p.load_model("../emotion_classifier/l2p_dance_model.json")
+        self._model_p.load_model("../emotion_classifier/l2p_model_final.json")
 
         self._model_a = xgb.XGBRegressor(verbosity=0)
-        self._model_a.load_model("../emotion_classifier/l2a_dance_model.json")
-
-        self.p_predictions = []
-        self.a_predictions = []
-        
-        self.normalizer = MinMaxScaler(feature_range=(0, 1), copy=True)
-
-        self.max_p = 0
-        self.max_p_i = -1 # index
-        self.max_a = 0
-        self.max_a_i = -1 # index
-
-        self.p_pos_count = 0
-        self.p_neg_count = 0
-
-        self.a_pos_count = 0
-        self.a_neg_count = 0
+        self._model_a.load_model("../emotion_classifier/l2a_model_final.json")
 
         self.predicted_p = 0.0
         self.predicted_a = 0.0
 
-
     def predict_emotion_coordinates(self, lma_features):    
-        """
-        Gets array of lma features and predicts each of their pleasure and arousal
-        """
-          
-                
+        p_predictions = []
+        a_predictions = []
+        
+        normalizer = MinMaxScaler(feature_range=(0, 1), copy=True)
+
         rows = []
         
         for entry in lma_features:
@@ -62,8 +44,8 @@ class EmotionClassifier():
 
 
         # Normalize data
-        self.normalizer.fit(rows)
-        rows = self.normalizer.transform(rows)
+        normalizer.fit(rows)
+        rows = normalizer.transform(rows)
 
         df = pd.DataFrame(rows, columns=['avg_hand_distance', 'avg_l_hand_hip_distance', 'avg_r_hand_hip_distance', 'avg_feet_distance', 'avg_l_hand_chest_distance', 'avg_r_hand_chest_distance', 'avg_l_elbow_hip_distance', 'avg_r_elbow_hip_distance', 'avg_chest_pelvis_distance', 'avg_neck_chest_distance', 'avg_neck_rotation_w', 'avg_neck_rotation_x', 'avg_neck_rotation_y', 'avg_neck_rotation_z',
          'avg_pelvis_rotation_w', 'avg_pelvis_rotation_x', 'avg_pelvis_rotation_y', 'avg_pelvis_rotation_z', 'std_l_hand_position', 'std_r_hand_position', 'avg_l_forearm_velocity', 
@@ -71,77 +53,54 @@ class EmotionClassifier():
          'avg_l_foot_velocity_y', 'avg_l_foot_velocity_z', 'avg_r_foot_velocity_x', 'avg_r_foot_velocity_y', 'avg_r_foot_velocity_z', 'avg_upper_body_volume', 
          'avg_distance_traveled'])
 
-        y_p = self._model_p.predict(df)
-        y_a = self._model_a.predict(df)
 
-        #for i in range(len(y_p)):
-        #    self.p_predictions.append(y_p[i])
-        #    self.a_predictions.append(y_a[i])
-        #
-        #    if(abs(y_p[i]) > self.max_p):
-        #        self.max_p = abs(y_p[i])
-        #    if(abs(y_a[i]) > self.max_a):
-        #        self.max_a = abs(y_a[i])
+        pos_p = 0
+        neg_p = 0
+        pos_a = 0
+        neg_a = 0
 
+        for i in range(len(rows)):
+            row=df.iloc[i]
+            #print(row)
+            
+            x = np.asarray([row])
 
-        self.predicted_p = sum(y_p)/len(y_p)
-        self.predicted_a = sum(y_a)/len(y_a)
+            y_p = self._model_p.predict(x)
+            y_a =  self._model_a.predict(x)
+            
+            """
+            if y_p[0] > 0:
+                pos_p +=1
+            else:
+                neg_p += 1
 
-        if(abs(self.predicted_p) > self.max_p):
-            self.max_p = abs(self.predicted_p)
-            self.max_p_i = len(self.p_predictions)
-        if(abs(self.predicted_a) > self.max_a):
-            self.max_a = abs(self.predicted_a)
-            self.max_a_i = len(self.a_predictions)
+            if y_a[0] > 0:
+                pos_a +=1
+            else:
+                neg_a += 1
+            """
 
-        if(self.predicted_p > 0):
-            self.p_pos_count += 1
-        else:
-            self.p_neg_count += 1
+            p_predictions.append(y_p[0])
+            a_predictions.append(y_a[0])
 
-        if(self.predicted_a > 0):
-            self.a_pos_count += 1
-        else:
-            self.a_neg_count += 1
+        """
+        for i in range(len(p_predictions)):
+            if(pos_p > neg_p):
+                if(p_predictions[i] < 0):
+                    p_predictions[i] = 0.0
+            if(pos_p < neg_p):
+                if(p_predictions[i] > 0):
+                    p_predictions[i] = 0.0
 
-        self.p_predictions.append(self.predicted_p)
-        self.a_predictions.append(self.predicted_a)
+            if(pos_a > neg_a):
+                if(a_predictions[i] < 0):
+                    a_predictions[i] = 0.0
+            if(pos_a < neg_a):
+                if(a_predictions[i] > 0):
+                    a_predictions[i] = 0.0
+        """
 
-
-        return (self.predicted_p, self.predicted_a)
-
-    def predict_final_emotion(self):
-        largest_p = self.p_predictions[self.max_p_i]
-        largest_a = self.a_predictions[self.max_a_i]
-
-        if((self.p_pos_count > self.p_neg_count and largest_p > 0) or (self.p_neg_count > self.p_pos_count and largest_p < 0)):
-            self.p_predictions.pop(self.max_p_i)
-            self.predicted_p = (sum(self.p_predictions)/len(self.p_predictions)) * 0.5 + largest_p * 0.5
-        else:
-            self.predicted_p = sum(self.p_predictions)/len(self.p_predictions)
-
-        if((self.a_pos_count > self.a_neg_count and largest_a > 0) or (self.a_neg_count > self.a_pos_count and largest_a < 0)):
-            self.a_predictions.pop(self.max_a_i)
-            self.predicted_a = (sum(self.a_predictions)/len(self.a_predictions)) * 0.5 + largest_a * 0.5
-        else:
-            self.predicted_a = sum(self.a_predictions)/len(self.a_predictions)
+        self.predicted_p = sum(p_predictions)/len(p_predictions)
+        self.predicted_a = sum(a_predictions)/len(a_predictions)
 
         return (self.predicted_p, self.predicted_a)
-
-    def clear(self):
-        self.p_predictions = []
-        self.a_predictions = []
-        
-        self.max_p = 0
-        self.max_p_i = -1 # index
-        self.max_a = 0
-        self.max_a_i = -1 # index
-
-        self.p_pos_count = 0
-        self.p_neg_count = 0
-
-        self.a_pos_count = 0
-        self.a_neg_count = 0
-
-        self.predicted_p = 0.0
-        self.predicted_a = 0.0
